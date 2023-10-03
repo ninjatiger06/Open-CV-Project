78127f84bed5d2d7b176d7572b8ac247fe9ff4c9
ninja_tiger06 <jpfefferman@gmail.com>
2023-10-02 23:03:41 -0700

    Finished plate swapping, commented
    Cleared bad images too

--- main.py
+++ main.py
@@ -4,46 +4,69 @@ import argparse
 import cv2
 import imutils
 
-def validateInput(prompt):
-	while True:
-		userInput = input(prompt)
-		if userInput.lower() == "y" or userInput.lower() == "n" or userInput.lower() == "yes" or userInput.lower() == "no":
-			return userInput.lower()
-		else:
-			print("Please input either 'y', 'n', 'yes', or 'no'.\n")
+"""
+	Description: Program identifies car license plates from images and can blur
+				plates or swap the plates in different images.
+	Authors: Jonas Pfefferman '24 and Kenan Begovic '24
+	Date: 9/15/23
+"""
 
-def loadImage():
-	ap = argparse.ArgumentParser()
-	ap.add_argument("-i", "--image", required=True, help="Path to Image")
-	args = vars(ap.parse_args())
-	image = cv2.imread(args["image"])
-	imgPath = args["image"]
-
-	IS_PNG = False
+def checkPNG(image, imgPath):
+	"""
+	Purpose: Checks to see if the image is a png. If not, saves the image as a png.
+	Parameters: The image as interpreted by cv2, the path to the image
+	Returns: The image object (in png form if not already)
+	"""
+	# Checks to see image isn't already png
 	if imgPath[len(imgPath)-4:] != ".png":
-		# splitPath = imgPath.split("/")
-		# imgName = splitPath[len(splitPath)-1]
-		# imgName = imgName[:len(imgName)-4]
-		if imgPath[len(imgPath)-5:] == "webp":
-			imgName = imgPath[len(imgPath)-5:]
-			imgPath = imgPath[len(imgPath)-4:] + "png"
+		if imgPath[len(imgPath)-4:] == "webp":
+			# imgName = imgPath[len(imgPath)-4:]
+			imgPath = imgPath[:len(imgPath)-4] + "png"
 		else:
-			imgName = imgPath[:len(imgPath)-4]
+			# imgName = imgPath[len(imgPath)-3:]
 			imgPath = imgPath[:len(imgPath)-3] + "png"
-		cv2.imwrite(f"{imgName}.png", image)
-		print(imgPath)
+		# Re-saves image as png
+		cv2.imwrite(imgPath, image)
 		image = cv2.imread(imgPath)
 
 	return image
 
+def loadImage():
+	"""
+	Purpose: Loads images from command line arguments
+	Parameters: None
+	Returns: The image objects
+	"""
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-i", "--image", required=True, help="Path to Image")
+	ap.add_argument("-im2", "--im2", required=True, help="Path to image 2")
+	args = vars(ap.parse_args())
+
+	image = cv2.imread(args["image"])
+	imgPath = args["image"]
+	image = checkPNG(image, imgPath)
+
+	im2 = cv2.imread(args["im2"])
+	im2Path = args["im2"]
+	im2 = checkPNG(im2, im2Path)
+
+	return image, im2
+
 def detectEdge(image) -> None:
+	"""
+	Purpose: Detects the rectangle of characters that forms a license plate and
+			 identifies it for the user
+	Parameters: The image object
+	Returns: The coordinates of the license plate, as a list of lists (I know
+			 better data structures exist, but this one just was easy and made sense.)
+	"""
 	# rectangle and square structuring overlays
 	rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (13, 5))
 	sqKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 21))
 
 	image = imutils.resize(image, height=700)
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	blurred = cv2.GaussianBlur(gray, (11, 11), 0)
+	# blurred = cv2.GaussianBlur(gray, (11, 11), 0)
 
 	# edged = cv2.Canny(blurred, 30, 150)
 
@@ -76,22 +99,49 @@ def detectEdge(image) -> None:
 	
 	# draw the rectangle
 	(x, y, w, h) = cv2.boundingRect(cnts[0])
-	box = image[y - 20:y + h + 20, x - 20:x + w + 20].copy()
+	box = image[y - 10:y + h + 10, x - 10:x + w + 10].copy()
 	# box = image.copy()
-	cv2.rectangle(image, (x - 20, y - 20), (x + w + 20, y + h + 20), (0, 255, 0), 2)
+	cv2.rectangle(image, (x - 10, y - 10), (x + w + 10, y + h + 10), (0, 255, 0), 2)
 	cv2.imshow("Identified", image)
 
-	cv2.imshow("rect", box)
+	cv2.imshow("Plate", box)
 
-	return [[x-20, y-20], [x + w + 20, y + h + 20]]
+	return [[x-10, y-10], [x + w + 10, y + h + 10]]
 
 def blurImage(image, boxCoords):
-	BLUR = validateInput("Would you like to blur? (y/n) ")
+	"""
+	Purpose: Blurs out the license plate beyond recognition
+	Parameters: The image of the car and the coordinates of the plate
+	Returns: The blurred plate image
+	"""
+	# Blurs only the part of the image identified as the license plate
+	image[boxCoords[0][1]: boxCoords[1][1], boxCoords[0][0]: boxCoords[1][0]] = cv2.blur(image[boxCoords[0][1]: boxCoords[1][1], boxCoords[0][0]: boxCoords[1][0]], (30, 30))
 
-	BLUR = True
-	if BLUR:
-		image[boxCoords[0][1]: boxCoords[1][1], boxCoords[0][0]: boxCoords[1][0]] = cv2.blur(image[boxCoords[0][1]: boxCoords[1][1], boxCoords[0][0]: boxCoords[1][0]], (20, 20))
-		cv2.imshow("Blurred", image)
+	cv2.imshow("Blurred", image)
+
+	return image
+
+def swapPlates(image, im2, boxCoords, boxCoords2):
+	"""
+	Purpose: Creates a new image in which the license plate of the second car is
+			 overlaid onto where the license plate of the first car is
+	Parameters: The images of both cars, the coordiantes of both plates
+	Returns: The new image
+	"""
+	# Grabs just the second license plate
+	plate2 = im2[boxCoords2[0][1]: boxCoords2[1][1], boxCoords2[0][0]: boxCoords2[1][0]]
+
+	# Determines the space needed to be filled on the original image
+	plateBoxX = boxCoords[1][0] - boxCoords[0][0]
+	plateBoxY = boxCoords[1][1] - boxCoords[0][1]
+
+	# Resizes second license plate to fit the hole where the first would be
+	plate2 = cv2.resize(plate2, (plateBoxX, plateBoxY))
+
+	image[boxCoords[0][1]: boxCoords[1][1], boxCoords[0][0]: boxCoords[1][0]] = plate2
+	cv2.imshow("Swapped Plates", image)
+
+	return image
 
 def featureMapping(image1, image2):
 
@@ -138,16 +188,26 @@ def confidenceFactor():
 
 def main():
 
-	image = loadImage()
+	image, im2 = loadImage()
 	image = imutils.resize(image, height=700)
-	# cv2.imshow("Original Image", image)
+	im2 = imutils.resize(im2, height=700)
 
 	boxCoords = detectEdge(image)
+	cv2.waitKey(0)
 
-	blurImage(image, boxCoords)
+	boxCoords2 = detectEdge(im2)
 
 	cv2.waitKey(0)
 
+	blurred = blurImage(image, boxCoords)
+	# cv2.imwrite("./images/blurredPlate.png", blurred)
+
+	cv2.waitKey(0)
+
+	swappedPlates = swapPlates(image, im2, boxCoords, boxCoords2)
+	# cv2.imwrite("./images/swappedPlates.png", swappedPlates)
+
+	cv2.waitKey(0)
 
 
 main()
